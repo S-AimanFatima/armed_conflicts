0c293ff32dba88e8a76e0592bcba8bb4cdf85958
S-AimanFatima <syedaaiman.fatima@mail.utoronto.ca>
2024-09-16 12:01:34 -0400

    Initial commit

---
+++ R/Week 2_InClass.R
@@ -0,0 +1,36 @@
+#CHL8010 In-Class Activity Week 2
+#Name: Syeda Aiman Fatima
+#Date: September 16, 2024
+
+library(here)
+library(tidyverse)
+
+#read in external data
+rawdat <- read.csv(here("Original", "maternalmortality.csv"), 
+                   header = TRUE)
+
+#create subset
+subset <- rawdat %>%
+  select(Country.Name, X2000:X2019)
+
+#change data format from wide to long
+df_long <- subset %>%
+  pivot_longer(cols = X2000:X2019,  # Select the year columns
+               names_to = "Year",   # Rename the column to 'Year'
+               names_prefix = "X",  # Remove the prefix 'X'
+               values_to = "MatMor") %>%  # Rename the values to 'MatMor'
+  mutate(Year = as.numeric(Year))  # Convert 'Year' to numeric
+
+#Introduce Git
+
+install.packages("usethis")
+library(usethis) 
+
+usethis::use_git_config(user.name = "S-AimanFatima", user.email = "syedaaiman.fatima@mail.utoronto.ca")
+
+# to confirm, generate a git situation-report, your user name and email should appear under Git config (global)
+usethis::git_sitrep()
+
+usethis::use_git()
+usethis::create_github_token()
+usethis::use_github()
